0454e5c76a4697372e57da05df1ecca5d1d19271
Henrik Büller <carl.henrik.buller@gmail.com>
2021-11-02 17:26:20 +0100

    Initialize Spring Boot

---
+++ src/main/java/com/lombardy/crema/CremaApplication.java
@@ -0,0 +1,13 @@
+package com.lombardy.crema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CremaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CremaApplication.class, args);
+	}
+
+}
