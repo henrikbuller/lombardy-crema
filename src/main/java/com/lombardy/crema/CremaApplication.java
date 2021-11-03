fb95452e6ff6416c6537a7482e9f30c4c184f144
Lukas Blomdell <lukasblomdell@gmail.com>
2021-11-03 10:19:33 +0100

    Exclude Datasource while no database is configured.

--- src/main/java/com/lombardy/crema/CremaApplication.java
+++ src/main/java/com/lombardy/crema/CremaApplication.java
@@ -2,8 +2,14 @@ package com.lombardy.crema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+
+// exclude DataSourceAutoConfiguration.class is only there while we dont have a database configured.
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class CremaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/lombardy/crema/controller/HelloController.java
+++ src/main/java/com/lombardy/crema/controller/HelloController.java
@@ -6,10 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
-
 }
